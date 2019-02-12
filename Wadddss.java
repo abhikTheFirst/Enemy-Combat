ddc84a2859aa61360e6be3229bb4d91a261b1e41
abhikTheFirst <35345368+abhikTheFirst@users.noreply.github.com>
2019-02-12 16:42:16 -0600

    Delete Wadddss.java

--- Wadddss.java
+++
@@ -1,8 +0,0 @@
-package oneSixWeeks;
-
-public class Wadddss {
-    public static void main(String[] args) {
-        System.out.println("Hello World! Super Push Test");
-        //hey loser this file is for testing so we may as well delete it because testing is for p****s
-    }
-}
