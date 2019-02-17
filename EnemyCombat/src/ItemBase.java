3e9681f54ad6e4781758fd8472030a5ee4c9d977
Abhik Kumar <arc.dawn007@gmail.com>
2019-02-17 17:12:35 -0600

    juhgetr

--- EnemyCombat/src/ItemBase.java
+++ EnemyCombat/src/ItemBase.java
@@ -1,7 +1,6 @@
 public class ItemBase{
   public static void main(String[] args){
     //item base commit stuff
+    System.out.println("stuff");
   }
-}
-
 }
--- EnemyCombat/src/Test.java
+++ EnemyCombat/src/Test.java
@@ -2,4 +2,8 @@
 public class Test {
   //This is a testing commit, don't worry about it
   //Time #2
+  public static void main(String[] args){
+
+    System.out.println("waffles");
+  }
 }
