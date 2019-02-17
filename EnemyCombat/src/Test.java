54831c013a6667a7ead3ca564d5e088f1f594507
Abhik Kumar <arc.dawn007@gmail.com>
2019-02-16 23:00:53 -0600

    Character commit#1

--- EnemyCombat/src/Test.java
+++ EnemyCombat/src/Test.java
@@ -2,5 +2,4 @@
 public class Test {
   //This is a testing commit, don't worry about it
   //Time #2
-
 }
---
+++ EnemyCombat/src/TestingWaffles.java
@@ -0,0 +1,5 @@
+public class TestingWaffles{
+  public static void main(String[] args){
+  //character commit testing #1  
+  }
+}
