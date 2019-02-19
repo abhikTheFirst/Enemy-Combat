113b2032c2941dfbb126f91bc7920c9238d33187
ritz_908411 <ritz_908411@VHC41717D7169D.leanderisd.org>
2019-02-19 16:27:58 -0600

    this is where all the gamers will be made

--- EnemyCombat/src/Character.java
+++ EnemyCombat/src/Character.java
@@ -1,5 +1,28 @@
-public class Character{
-  public static void main(String[] args){
-  //character commit testing #2
-  }
+public class abstract Character{
+	private String name;
+	private int health;
+	
+	public Character() {
+		name = "Gamer";
+		health = 1337;
+	}
+	
+	public Character(String n, int h) {
+		name = n;
+		health = h;
+	}
+	
+	public void setName(String n) {
+		name = n;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	public void setHealth(int h) {
+		health = h;
+	}
+	public int getHealth() {
+		return health;
+	}
 }
