f668ffa17ea4d33c4eed2dfd246b795c444fcedb
xande <xande@10.3.85.247>
2019-02-25 15:08:02 -0600

    fixed input scanner issue added wait() to consolidate try/catch for thread.sleep()

--- EnemyCombat/src/RunnerBase.java
+++ EnemyCombat/src/RunnerBase.java
@@ -3,6 +3,7 @@ import static java.lang.System.*;
 
 public class RunnerBase{
   
+	private static Scanner input;
 	public static void main(String[] args){
 		runGame();
 	}
@@ -15,29 +16,23 @@ public class RunnerBase{
 	}
 	
 	public static boolean printStart(){
-		try {
-			Scanner input = new Scanner(System.in);
-			newScreen();
-			out.println("▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲");
-			Thread.sleep(170);
-			out.println("▲▼▲▼▲▼ ENEMY COMBAT ▼▲▼▲▼▲");
-			Thread.sleep(170);
-			out.println("▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼\n");
-			Thread.sleep(170);
-			printSlow("  1 >>	START\n", 80);
-			printSlow("  2 >>	QUIT\n\n", 80);
-			printSlow("  CHOOSE --> ", 80);
-			
-			switch(input.nextInt()) {
-			case(1): 
-				return true;
-			default:
-				out.println("\n<< GOODBYE. >>");
-				return false;
-			}
-		}
-		catch(InterruptedException e) {
-			out.println("#### TIMING ERROR ####");
+		input = new Scanner(System.in);
+		newScreen();
+		out.println("▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲");
+		wait(170);
+		out.println("▲▼▲▼▲▼ ENEMY COMBAT ▼▲▼▲▼▲");
+		wait(170);
+		out.println("▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼\n");
+		wait(170);
+		printSlow("  1 >>	START\n", 80);
+		printSlow("  2 >>	QUIT\n\n", 80);
+		printSlow("  CHOOSE --> ", 80);
+		
+		switch(input.nextInt()) {
+		case(1): 
+			return true;
+		default:
+			out.println("\n<< GOODBYE. >>");
 			return false;
 		}
 	}
@@ -46,25 +41,22 @@ public class RunnerBase{
 		
 		newScreen();
 		
-		Scanner input = new Scanner(System.in);
+		input = new Scanner(System.in);
 		
 		printSlow("Hello! Welcome To Enemy Combat!\n\n",80);
 		printSlow("What Is The Name Of Your Champion --> ",80);
 		String name = input.nextLine();
 		printSlow("\n\n",100); printSlow(name + "! What A Stupid Name!\n\n",80);
+		
+		printSlow("",80);
 	}
 	
 	public static void printSlow(String input,int speed){
-		try {
-			String[] h = input.split("");
+		String[] h = input.split("");
 			
-			for(String x: h) {
-				out.print(x);
-				Thread.sleep(speed);
-			}
-		}
-		catch(InterruptedException e){
-			out.println("#### TIMING ERROR ####");
+		for(String x: h) {
+			out.print(x);
+			wait(speed);
 		}
 	}
 	public static void newScreen() {
@@ -74,5 +66,13 @@ public class RunnerBase{
 				+ "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
 				+ "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
 				+ "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	}
+	public static void wait(int time) {
+		try {
+			Thread.sleep(time);
+		}
+		catch(InterruptedException e){
+			out.println("#### TIMING ERROR ####");
+		}
 	}
 }
