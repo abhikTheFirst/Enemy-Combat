a7b63a384c8c63de6acf392475077be9a996e42b
gennarel_883634 <gennarel_883634@VHC41704D7173D.leanderisd.org>
2019-02-26 11:50:54 -0600

    re-added wait()

--- EnemyCombat/src/RunnerBase.java
+++ EnemyCombat/src/RunnerBase.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import static java.lang.System.*;
 
 public class RunnerBase{
-  
+	
 	private static Scanner input;
 	
 	public static void main(String[] args){
@@ -18,35 +18,28 @@ public class RunnerBase{
 	}
 	
 	public static boolean printStart(){
-		try {
-			input = new Scanner(System.in);
-			newScreen();
-			out.println("▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲");
-			Thread.sleep(170);
-			out.println("▲▼▲▼▲▼ ENEMY COMBAT ▼▲▼▲▼▲");
-			Thread.sleep(170);
-			out.println("▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼\n");
-			Thread.sleep(170);
-			printSlow("  1 >>	START\n", 80);
-			printSlow("  2 >>	QUIT\n\n", 80);
-			printSlow("  CHOOSE --> ", 80);
-			
-			switch(input.nextInt()) {
-			case(1): 
-				return true;
-			default:
-				out.println("\n<< GOODBYE. >>");
-				return false;
-			}
-		}
-		catch(InterruptedException e) {
-			out.println("#### TIMING ERROR ####");
+		input = new Scanner(System.in);
+		newScreen();
+		out.println("▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲");
+		wait(170);
+		out.println("▲▼▲▼▲▼ ENEMY COMBAT ▼▲▼▲▼▲");
+		wait(170);
+		out.println("▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼\n");
+		wait(170);
+		printSlow("  1 >>	START\n", 80);
+		printSlow("  2 >>	QUIT\n\n", 80);
+		printSlow("  CHOOSE --> ", 80);
+		
+		switch(input.nextInt()) {
+		case(1): 
+			return true;
+		default:
+			out.println("\n<< GOODBYE. >>");
 			return false;
 		}
 	}
 	
 	public static void postStartText(){
-		
 		newScreen();
 		
 		input = new Scanner(System.in);
@@ -58,18 +51,14 @@ public class RunnerBase{
 	}
 	
 	public static void printSlow(String input,int speed){
-		try {
-			String[] h = input.split("");
-			
-			for(String x: h) {
-				out.print(x);
-				Thread.sleep(speed);
-			}
-		}
-		catch(InterruptedException e){
-			out.println("#### TIMING ERROR ####");
+		String[] h = input.split("");
+					
+		for(String x: h) {
+			out.print(x);
+			wait(speed);
 		}
 	}
+	
 	public static void newScreen() {
 		out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
 				+ "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
@@ -77,5 +66,14 @@ public class RunnerBase{
 				+ "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
 				+ "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
 				+ "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	}
+	
+	public static void wait(int time) {
+		try {
+			Thread.sleep(time);
+		}
+		catch(InterruptedException e) {
+			out.println("#### TIMING ERROR ####");
+		}
 	}
 }
