79b34012f099fc69a572d061b2fa91af20e036ff
Alan <jordan.camilletti@gmail.com>
2019-02-26 16:22:33 -0600

    Fixed character conflicts

--- EnemyCombat/src/Character.java
+++ EnemyCombat/src/Character.java
@@ -38,32 +38,3 @@ public class Character{
 	  return(name+" is level "+lvl+" and has "+currHealth+" of "+health+" health remaining");
   }
 }
-=======
-public class abstract Character{
-	private String name;
-	private int health;
-	
-	public Character() {
-		name = "Gamer";
-		health = 1337;
-	}
-	
-	public Character(String n, int h) {
-		name = n;
-		health = h;
-	}
-	
-	public void setName(String n) {
-		name = n;
-	}
-	public String getName() {
-		return name;
-	}
-	
-	public void setHealth(int h) {
-		health = h;
-	}
-	public int getHealth() {
-		return health;
-	}
-}
