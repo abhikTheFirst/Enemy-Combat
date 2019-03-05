799f51d74f192afecfb7957b2d5434d1bbe3edf4
JudetheDud3 <47370378+JudetheDud3@users.noreply.github.com>
2019-03-05 16:55:25 -0600

    Added instance data, modifiers and accessors.

--- EnemyCombat/src/ItemBase.java
+++ EnemyCombat/src/ItemBase.java
@@ -1,7 +1,34 @@
 public class ItemBase{
-  public static void main(String[] args){
-    System.out.println("Xander is gay");
-    //item base commit stuff
+private double lootChance;
+private int value;
+private String name;
+  public double getLootChance()
+  {
+    return lootChance;
+  }
 
+  public void setLootChance(double l)
+  {
+    lootChance = l;
+  }
+
+  public int getValue()
+  {
+    return value;
+  }
+
+  public void setValue(int v)
+  {
+     value = v;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public vod setName()
+  {
+    name = n;
   }
 }
